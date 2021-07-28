75ed916406299d1413e75d45ce01f9bf7f257108
talamcol <71456764+talamcol@users.noreply.github.com>
2021-07-28 15:17:24 +0200

    feat: validate config

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,8 @@
-import path from "path"
-import fs from "fs"
-
 import { unknown } from "./unknown"
+import { loadConfiguration } from "./loadConfiguration"
 import { Debugger } from "./debugger"
 import { clean } from "./clean"
 import { build } from "./build"
-
-interface IndexFile {
-  path: string
-  extension: "tsx" | "svg"
-}
-
-export interface AssetsConfig {
-  rootPath: string
-  indexFiles?: IndexFile[]
-  replaceColors?: string[]
-  debug?: boolean
-}
-
-const loadConfiguration = (): AssetsConfig => {
-  const configPath = path.join(process.cwd(), "assets.config.json")
-
-  if (fs.existsSync(configPath)) {
-    return JSON.parse(fs.readFileSync(configPath).toString())
-  } else {
-    return {
-      rootPath: "./assets",
-      indexFiles: [],
-      replaceColors: [],
-      debug: false,
-    }
-  }
-}
 
 export default function run(args) {
   const selectedCommand = args[2]
---
+++ src/loadConfiguration.ts
@@ -0,0 +1,96 @@
+import path from "path"
+import fs from "fs"
+
+interface IndexFile {
+  path: string
+  extension: "tsx" | "svg"
+}
+
+export interface AssetsConfig {
+  rootPath: string
+  indexFiles?: IndexFile[]
+  replaceColors?: string[]
+  debug?: boolean
+}
+
+export const loadConfiguration = (): AssetsConfig => {
+  const configPath = path.join(process.cwd(), "assets.config.json")
+
+  if (fs.existsSync(configPath)) {
+    const parsedConfig = JSON.parse(fs.readFileSync(configPath).toString())
+    validateConfiguration(parsedConfig)
+    return parsedConfig
+  } else {
+    return {
+      rootPath: "./assets",
+      indexFiles: [],
+      replaceColors: [],
+      debug: false,
+    }
+  }
+}
+
+export const validateConfiguration = (config) => {
+  const { rootPath, indexFiles, replaceColors, debug } = config || {}
+  const errors = []
+
+  rootPath && validateRootPath(rootPath, errors)
+  indexFiles && validateIndexFiles(indexFiles, errors)
+  replaceColors && validateReplaceColors(replaceColors, errors)
+  debug && validateDebug(debug, errors)
+
+  if (errors.length) {
+    console.error("assets-pkg is not configured correctly")
+    errors.forEach((err) => console.error(err))
+    process.exit(1)
+  }
+}
+
+const validateRootPath = (rootPath, errors: string[]) => {
+  if (typeof rootPath !== "string") {
+    errors.push("rootPath must be a string")
+  }
+}
+
+const validateIndexFiles = (indexFiles, errors: string[]) => {
+  if (!Array.isArray(indexFiles)) {
+    errors.push("indexFiles must be an array")
+  }
+  if (
+    indexFiles.length &&
+    !indexFiles.every(
+      (el) =>
+        el &&
+        el.path &&
+        el.extension &&
+        typeof el.path === "string" &&
+        ["tsx", "svg"].includes(el.extension)
+    )
+  ) {
+    errors.push(
+      "Wrong structure for indexFiles. Please provide an array with the structure [{ path: string, extension: tsx | svg }]"
+    )
+  }
+}
+
+const validateReplaceColors = (replaceColors, errors: string[]) => {
+  if (!Array.isArray(replaceColors)) {
+    errors.push("replaceColors must be an array")
+  }
+  if (
+    replaceColors.length &&
+    !replaceColors.every(
+      (el) => el && typeof el === "string" && el.startsWith("#")
+    )
+  ) {
+    errors.push(
+      "Wrong structure for replaceColors. Please provide an array containing HEX color strings"
+    )
+  }
+}
+
+const validateDebug = (debug, errors: string[]) => {
+  if (typeof debug !== "boolean") {
+    errors.push("debug must be a boolean")
+  }
+}
