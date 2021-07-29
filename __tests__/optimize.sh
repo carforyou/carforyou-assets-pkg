f385ed15c8260e101da85af35b0a92980862d96e
talamcol <71456764+talamcol@users.noreply.github.com>
2021-07-29 14:51:09 +0200

    fix: test

--- __tests__/optimize.sh
+++ __tests__/optimize.sh
@@ -8,9 +8,9 @@ else
     exit 1
 fi
 
-NOT_OPTIMIZED_FILE_SIZE=$(stat -f%z ./assets/src/instagram.svg)
-OPTIMIZED_FILE_SIZE=$(stat -f%z ./assets/dist/instagram.svg)
-if [[ "$NOT_OPTIMIZED_FILE_SIZE" -gt "$OPTIMIZED_FILE_SIZE" ]]; then
+NOT_OPTIMIZED_FILE_SIZE=$(wc -c <./assets/src/instagram.svg)
+OPTIMIZED_FILE_SIZE=$(wc -c <./assets/dist/instagram.svg)
+if [ "$NOT_OPTIMIZED_FILE_SIZE" -gt "$OPTIMIZED_FILE_SIZE" ]; then
     echo "PASS (optimize): optimized file is smaller than original."
 else
     echo "FAILED (optimize): optimized file is not smaller than original."
